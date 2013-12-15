eee74dc343b6430c6368c8e63f68c1a400f77afa
xyrix <xyrixorg@gmail.com>
2013-12-15 02:29:37 +0000

    enemy now accelerates really dumbly to player

--- enemy.js
+++ enemy.js
@@ -6,6 +6,8 @@ function Enemy(x, y) {
 	this._bullets.setAll('outOfBoundsKill', true);
 	this._sprite = game.add.sprite(x, y, 'enemy');
 	this._sprite.anchor.setTo(0.5, 0.5);
+	this._sprite.body.maxVelocity.x = 250;
+	this._sprite.body.maxVelocity.y = 250;
 }
 
 Enemy.prototype.update = function() {
@@ -13,5 +15,20 @@ Enemy.prototype.update = function() {
 	var y = Math.floor(this._sprite.y / 512);
 	var px = Math.floor(player.x / 512);
 	var py = Math.floor(player.y / 512);
-	var goal = pathFinder.nextStep(x, y, px, py);
+	var gx, gy;
+	if (x != px || y != py) {
+		var goal = pathFinder.nextStep(x, y, px, py);
+		gx = goal.x * 512 + 256;
+		gy = goal.y * 512 + 256;
+	} else {
+		gx = player.x;
+		gy = player.y;
+	}
+	this._sprite.body.acceleration.x = gx - this._sprite.x;
+	this._sprite.body.acceleration.y = gy - this._sprite.y;
+	this._sprite.body.acceleration.normalize();
+	this._sprite.body.acceleration.x *= 250;
+	this._sprite.body.acceleration.y *= 250;
+	var goal = new Phaser.Point(gx, gy);
+	this._sprite.rotation = game.physics.angleBetween(this._sprite, goal);
 }
